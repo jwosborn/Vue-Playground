e16a45974268c5ab6f651b1ff0c4afab1417f038
kelseymegan92 <kelseymegan92@Kelseys-MacBook-Pro.local>
2019-05-21 14:39:58 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
+
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hello, W0rld!" />
+    <ItWorks/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
+    ItWorks,
     HelloWorld
   }
 }
+import ItWorks from './ItWorks.vue';
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <style>
---
+++ src/ItWorks.vue
@@ -0,0 +1,29 @@
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js"></script>
+<template>
+   <div id="root">
+       <ul>
+           <li>Test</li>
+           <li>Test</li>
+           <li>Test</li>
+       </ul>
+    </div> 
+</template>
+
+<script>
+export default {
+    name: 'ItWorks'
+}
+
+
+
+</script>
+
+<style>
+
+</style>
+
+
+
+
+
+
