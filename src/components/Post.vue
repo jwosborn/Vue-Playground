241fc216ef7952f469841df335d8ce76b189cbe7
kelseymegan92 <kelseymegan92@Kelseys-MacBook-Pro.local>
2019-12-13 12:56:03 -0500

    gas mileage addition

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,12 +1,22 @@
 <template>
-  <div id="post-wrapper">
-    <div class="toggle-block" @click="toggleBlock = !toggleBlock">Toggle Blue Block</div>
-    <div class="blue-block" v-if="toggleBlock"></div>
-    <p>{{ content }}</p>
-    <div class="count-up" @click="count++">+1</div>
-    <div class="count-down" @click="count--">-1</div>
-    <p>The count is {{count}}.</p>
+<div id="post-wrapper">
+  <div class="toggle-block" @click="toggleBlock = !toggleBlock">Toggle Blue Block</div>
+  <div class="blue-block" v-if="toggleBlock"></div>
+  <p>{{ content }}</p>
+  <div class="count-up" @click="count++">+1</div>
+  <div class="count-down" @click="count--">-1</div>
+  <p>The count is {{count}}.</p>
+  <div>
+    <span>Gas Mileage Calculator</span>
+    <div class="fuel">
+      <input name="fuel" type="text" :value="fuel"/>
+      <input name="miles" type="text" value="miles"/>
+      <p>
+        Your gas mileage is {{ mileage }}.
+      </p>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -16,7 +26,10 @@ export default {
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       toggleBlock: false,
-      count: 0
+count: 0,
+fuel: 0,
+miles: 0,
+mileage: this.miles/this.fuel,
     };
   },
   name: "Post"
